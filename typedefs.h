7c2fbeae358b152136ca7691ead565718ab4b2aa
tobi <>
1999-02-01 17:21:38 +0000

    64 bit fix

--- typedefs.h
+++ typedefs.h
@@ -35,8 +35,13 @@ typedef signed char sbyte;
 typedef unsigned short word;
 typedef signed short sword;
 
+#ifdef __alpha    // add other 64 bit systems here
+typedef unsigned int dword;
+typedef signed   int sdword;
+#else             // 32 and 16 bit machines
 typedef unsigned long dword;
 typedef signed long sdword;
+#endif
 
 #if !defined(UNIX) && !defined(SASC)
 typedef unsigned short ushort;
