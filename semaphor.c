02b6ea4b3953d8011934052223eabdb26fe3ae6b
oliver.grimm <>
2000-08-23 05:07:50 +0000

    Reincarnation of semaphor.c

---
+++ semaphor.c
@@ -0,0 +1,95 @@
+/*
+ *  SMAPI; Modified Squish MSGAPI
+ *
+ *  Squish MSGAPI0 is copyright 1991 by Scott J. Dudley.  All rights reserved.
+ *  Modifications released to the public domain.
+ *
+ *  Use of this file is subject to the restrictions contain in the Squish
+ *  MSGAPI0 licence agreement.  Please refer to licence.txt for complete
+ *  details of the licencing restrictions.  If you do not find the text
+ *  of this agreement in licence.txt, or if you do not have this file,
+ *  you should contact Scott Dudley at FidoNet node 1:249/106 or Internet
+ *  e-mail Scott.Dudley@f106.n249.z1.fidonet.org.
+ *
+ *  In no event should you proceed to use any of the source files in this
+ *  archive without having accepted the terms of the MSGAPI0 licensing
+ *  agreement, or such other agreement as you are able to reach with the
+ *  author.
+ */
+
+#include <unistd.h>
+
+#include "semaphor.h"
+
+#if defined(NOSEMAPHORES)
+
+/* No code for Non-Multitasking Systems */
+
+#elif definded(__BEOS__)
+
+void create_semaphore(SEMAPHORE *sem)
+{
+  *sem = create_sem(0,0);
+  release_sem(*sem);
+}
+
+void delete_semaphore(SEMAPHORE *sem)
+{
+  delete_sem(*sem);
+}
+
+void lock_semaphore(SEMAPHORE *sem)
+{
+  if (acquire_sem(*sem) != B_NO_ERROR)
+    snooze(10);
+}
+
+void unlock_semaphore(SEMAPHORE *sem)
+{
+  release_sem(*sem);
+}
+
+#elif defined(UNIX)
+
+void create_semaphore(SEMAPHORE *sem)
+{
+  *sem = semget(IPC_PRIVATE, 1, 0x1f|SEM_CREAT);
+  unlock_semaphore(sem);
+}
+
+void delete_semaphore(SEMAPHORE *sem)
+{
+#ifdef HAS_SEMUN
+  union semun fl;
+  fl.val = 0;
+#else
+  int fl = 0;
+#endif
+
+  semctl(*sem, 0, IPC_RMID, fl);
+}
+
+void lock_semaphore(SEMAPHORE *sem)
+{
+  struct sembuf sops;
+  
+  sops.sem_num = 0;
+  sops.sem_op  = -1;
+  sops.sem_flg = 0;
+
+  while (semop(*sem, &sops, 1))
+    usleep(10);
+}
+
+void unlock_semaphore(SEMAPHORE *sem)
+{
+  struct sembuf sops;
+  
+  sops.sem_num = 0;
+  sops.sem_op  = 1;
+  sops.sem_flg = 0;
+
+  semop(*sem, &sops, 1);
+}
+
+#endif
---
+++ semaphor.h
@@ -0,0 +1,64 @@
+/*
+ *  SMAPI; Modified Squish MSGAPI
+ *
+ *  Squish MSGAPI0 is copyright 1991 by Scott J. Dudley.  All rights reserved.
+ *  Modifications released to the public domain.
+ *
+ *  Use of this file is subject to the restrictions contain in the Squish
+ *  MSGAPI0 licence agreement.  Please refer to licence.txt for complete
+ *  details of the licencing restrictions.  If you do not find the text
+ *  of this agreement in licence.txt, or if you do not have this file,
+ *  you should contact Scott Dudley at FidoNet node 1:249/106 or Internet
+ *  e-mail Scott.Dudley@f106.n249.z1.fidonet.org.
+ *
+ *  In no event should you proceed to use any of the source files in this
+ *  archive without having accepted the terms of the MSGAPI0 licensing
+ *  agreement, or such other agreement as you are able to reach with the
+ *  author.
+ */
+ 
+#ifndef __SEMAPHORE_H
+#define __SEMAPHORE_H
+
+#include "compiler.h"
+
+/* No semaphores (unsupported or non-multitasking systems) */
+#if defined(NOSEMAPHORES)
+
+#define SEMAPHORE int
+
+#define create_semaphore(s) 
+#define delete_semaphore(s)
+#define lock_semaphore(s)
+#define unlock_semaphore(s)
+
+/* BeOS */
+#elif defined(__BEOS__)
+
+#include <OS.h>
+
+#define SEMAPHORE sem_id
+
+/* System V semaphores */
+#elif defined(UNIX)
+
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+
+#define SEMAPHORE int
+
+#else
+
+#error "Don't know how to setup semaphore. Use -DNOSEMAPHORES"
+
+#endif
+
+#ifndef NOSEMAPHORES
+void create_semaphore(SEMAPHORE *sem);
+void delete_semaphore(SEMAPHORE *sem);
+void lock_semaphore(SEMAPHORE *sem);
+void unlock_semaphore(SEMAPHORE *sem);
+#endif
+
+#endif
