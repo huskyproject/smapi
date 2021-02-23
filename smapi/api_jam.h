a192ba38f12088ff47b9959e94eda437fef7ce4d
Michael Dukelsky <dukelsky@users.noreply.github.com>
2021-02-23 18:53:17 +0300

    indent values in #defines

--- smapi/api_jam.h
+++ smapi/api_jam.h
@@ -44,49 +44,50 @@ extern "C" {
 */
 typedef struct
 {
-    byte  Signature[4];               /* <J><A><M> followed by <NUL> */
-    dword DateCreated;                /* Creation date */
-    dword ModCounter;                 /* Last processed counter */
-    dword ActiveMsgs;                 /* Number of active (not deleted) msgs */
-    dword PasswordCRC;                /* CRC-32 of password to access */
-    dword BaseMsgNum;                 /* Lowest message number in index file */
-    dword highwater;                  /* Number of the last msg scanned */
-    byte  RSRVD[996];                 /* Reserved space */
+    byte  Signature[4]; /* <J><A><M> followed by <NUL> */
+    dword DateCreated;  /* Creation date */
+    dword ModCounter;   /* Last processed counter */
+    dword ActiveMsgs;   /* Number of active (not deleted) msgs */
+    dword PasswordCRC;  /* CRC-32 of password to access */
+    dword BaseMsgNum;   /* Lowest message number in index file */
+    dword highwater;    /* Number of the last msg scanned */
+    byte  RSRVD[996];   /* Reserved space */
 } JAMHDRINFO, *JAMHDRINFOptr;
 
 #define HDRINFO_SIZE (1000 + (4 * 6))
 /*
 **  Message status bits
 */
-#define JMSG_LOCAL 0x00000001L          /* Msg created locally */
-#define JMSG_INTRANSIT 0x00000002L      /* Msg is in-transit */
-#define JMSG_PRIVATE 0x00000004L        /* Private */
-#define JMSG_READ 0x00000008L           /* Read by addressee */
-#define JMSG_SENT 0x00000010L           /* Sent to remote */
-#define JMSG_KILLSENT 0x00000020L       /* Kill when sent */
-#define JMSG_ARCHIVESENT 0x00000040L    /* Archive when sent */
-#define JMSG_HOLD 0x00000080L           /* Hold for pick-up */
-#define JMSG_CRASH 0x00000100L          /* Crash */
-#define JMSG_IMMEDIATE 0x00000200L      /* Send Msg now, ignore restrictions */
-#define JMSG_DIRECT 0x00000400L         /* Send directly to destination */
-#define JMSG_GATE 0x00000800L           /* Send via gateway */
-#define JMSG_FILEREQUEST 0x00001000L    /* File request */
-#define JMSG_FILEATTACH 0x00002000L     /* File(s) attached to Msg */
-#define JMSG_TRUNCFILE 0x00004000L      /* Truncate file(s) when sent */
-#define JMSG_KILLFILE 0x00008000L       /* Delete file(s) when sent */
-#define JMSG_RECEIPTREQ 0x00010000L     /* Return receipt requested */
-#define JMSG_CONFIRMREQ 0x00020000L     /* Confirmation receipt requested */
-#define JMSG_ORPHAN 0x00040000L         /* Unknown destination */
-#define JMSG_ENCRYPT 0x00080000L        /* Msg text is encrypted */
-#define JMSG_COMPRESS 0x00100000L       /* Msg text is compressed */
-#define JMSG_ESCAPED 0x00200000L        /* Msg text is seven bit ASCII */
-#define JMSG_FPU 0x00400000L            /* Force pickup */
-#define JMSG_TYPELOCAL 0x00800000L      /* Msg is for local use only (not for export) */
-#define JMSG_TYPEECHO 0x01000000L       /* Msg is for conference distribution */
-#define JMSG_TYPENET 0x02000000L        /* Msg is direct network mail */
-#define JMSG_NODISP 0x20000000L         /* Msg may not be displayed to user */
-#define JMSG_LOCKED 0x40000000L         /* Msg is locked, no editing possible */
-#define JMSG_DELETED 0x80000000L        /* Msg is deleted */
+#define JMSG_LOCAL       0x00000001L /* Msg created locally */
+#define JMSG_INTRANSIT   0x00000002L /* Msg is in-transit */
+#define JMSG_PRIVATE     0x00000004L /* Private */
+#define JMSG_READ        0x00000008L /* Read by addressee */
+#define JMSG_SENT        0x00000010L /* Sent to remote */
+#define JMSG_KILLSENT    0x00000020L /* Kill when sent */
+#define JMSG_ARCHIVESENT 0x00000040L /* Archive when sent */
+#define JMSG_HOLD        0x00000080L /* Hold for pick-up */
+#define JMSG_CRASH       0x00000100L /* Crash */
+#define JMSG_IMMEDIATE   0x00000200L /* Send Msg now, ignore restrictions */
+#define JMSG_DIRECT      0x00000400L /* Send directly to destination */
+#define JMSG_GATE        0x00000800L /* Send via gateway */
+#define JMSG_FILEREQUEST 0x00001000L /* File request */
+#define JMSG_FILEATTACH  0x00002000L /* File(s) attached to Msg */
+#define JMSG_TRUNCFILE   0x00004000L /* Truncate file(s) when sent */
+#define JMSG_KILLFILE    0x00008000L /* Delete file(s) when sent */
+#define JMSG_RECEIPTREQ  0x00010000L /* Return receipt requested */
+#define JMSG_CONFIRMREQ  0x00020000L /* Confirmation receipt requested */
+#define JMSG_ORPHAN      0x00040000L /* Unknown destination */
+#define JMSG_ENCRYPT     0x00080000L /* Msg text is encrypted */
+#define JMSG_COMPRESS    0x00100000L /* Msg text is compressed */
+#define JMSG_ESCAPED     0x00200000L /* Msg text is seven bit ASCII */
+#define JMSG_FPU         0x00400000L /* Force pickup */
+#define JMSG_TYPELOCAL   0x00800000L /* Msg is for local use only 
+                                        (not for export) */
+#define JMSG_TYPEECHO    0x01000000L /* Msg is for conference distribution */
+#define JMSG_TYPENET     0x02000000L /* Msg is direct network mail */
+#define JMSG_NODISP      0x20000000L /* Msg may not be displayed to user */
+#define JMSG_LOCKED      0x40000000L /* Msg is locked, no editing possible */
+#define JMSG_DELETED     0x80000000L /* Msg is deleted */
 /*
 **  Message header
 */
@@ -118,27 +119,27 @@ typedef struct
 /*
 **  Message header subfield types
 */
-#define JAMSFLD_OADDRESS 0
-#define JAMSFLD_DADDRESS 1
-#define JAMSFLD_SENDERNAME 2
-#define JAMSFLD_RECVRNAME 3
-#define JAMSFLD_MSGID 4
-#define JAMSFLD_REPLYID 5
-#define JAMSFLD_SUBJECT 6
-#define JAMSFLD_PID 7
-#define JAMSFLD_TRACE 8
-#define JAMSFLD_ENCLFILE 9
+#define JAMSFLD_OADDRESS     0
+#define JAMSFLD_DADDRESS     1
+#define JAMSFLD_SENDERNAME   2
+#define JAMSFLD_RECVRNAME    3
+#define JAMSFLD_MSGID        4
+#define JAMSFLD_REPLYID      5
+#define JAMSFLD_SUBJECT      6
+#define JAMSFLD_PID          7
+#define JAMSFLD_TRACE        8
+#define JAMSFLD_ENCLFILE     9
 #define JAMSFLD_ENCLFWALIAS 10
-#define JAMSFLD_ENCLFREQ 11
-#define JAMSFLD_ENCLFILEWC 12
+#define JAMSFLD_ENCLFREQ    11
+#define JAMSFLD_ENCLFILEWC  12
 #define JAMSFLD_ENCLINDFILE 13
-#define JAMSFLD_EMBINDAT 1000
+#define JAMSFLD_EMBINDAT  1000
 #define JAMSFLD_FTSKLUDGE 2000
-#define JAMSFLD_SEENBY2D 2001
-#define JAMSFLD_PATH2D 2002
-#define JAMSFLD_FLAGS 2003
+#define JAMSFLD_SEENBY2D  2001
+#define JAMSFLD_PATH2D    2002
+#define JAMSFLD_FLAGS     2003
 #define JAMSFLD_TZUTCINFO 2004
-#define JAMSFLD_UNKNOWN 0xffff
+#define JAMSFLD_UNKNOWN   0xffff
 /*
 **  Message header subfield
 */
